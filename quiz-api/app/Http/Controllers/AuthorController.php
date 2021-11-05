91d304461c9af33bcb14d59c5024b5fd7282a507
Lyden Chai <lyden.chai@PNCLB12-23.pnc.lan>
2021-11-05 09:27:19 +0700

    finished at 9:27AM

---
+++ quiz-api/app/Http/Controllers/AuthorController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Author;
+use App\Http\Resources\AuthorResource;
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        return AuthorResource::collection(Author::get()->take(3));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'min:3|max:10',
+            'age' => 'min:1|max:10',
+            'province' => 'nullable'
+        ]);
+        $author = new Author();
+        $author->name = $request->name;
+        $author->age = $request->age;
+        $author->province = $request->province;
+
+        $author->save();
+        return response()->json(['message'=>'created', 'data'=> $author], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return new AuthorResource(Author::findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name'=>'min:3|max:10',
+            'age'=>'min:1|max:10',
+            'province'=>'nullable'
+        ]);
+        $author = Author::findOrFail($id);
+        $author->name = $request->name;
+        $author->age = $request->age;
+        $author->province = $request->province;
+
+        $author->save();
+        return response()->json(['massage'=>'Update', 'data'=> $author], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $isDeleted = Author::destroy($id);
+        if($isDeleted == 1){
+            return response()->json(['massage'=>'Deleted'], 200);
+        }else{
+            return response()->json(['massage'=>'Not Found'], 404);
+        }
+    }
+}
---
+++ quiz-api/app/Http/Controllers/BookController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+use App\Http\Resources\BookResource;
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return BookResource::collection(Book::latest()->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'min:3|max:10',
+            'body' => 'min:3|max:50'
+        ]);
+        $book = new Book();
+        $book->author_id = $request->author_id;
+        $book->title = $request->title;
+        $book->body = $request->body;
+        $book->save();
+
+        return response()->json(["Message" => "Created", "data" => $book], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Book::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title'=>'min:3|max:10',
+            'body'=>'min:3|max:50',
+        ]);
+        $book = Author::findOrFail($id);
+        $book->title = $request->title;
+        $book->body = $request->body;
+
+        $author->save();
+        return response()->json(['massage'=>'Update', 'data'=> $book], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $isDeleted = Author::destroy($id);
+        if($isDeleted == 1){
+            return response()->json(['massage'=>'Deleted'], 200);
+        }else{
+            return response()->json(['massage'=>'Not Found'], 404);
+        }
+    }
+}
